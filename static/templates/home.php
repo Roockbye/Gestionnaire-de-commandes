d6b7e0ca58abbdd66e21d85f5c70137b1bd89fb2
lucille <lucille.cenac@ynov.com>
2024-03-07 17:12:21 +0100

    page front home

---
+++ static/templates/home.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Accueil</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="../CSS/home.css" />
+</head>
+<body>
+
+<!-- header -->
+<header class="menu">
+    <nav>
+        <label class="logo">Restôt</label>
+        <ul>
+            <li><a href="#hero-section" class="active">Accueil</a></li>
+            <li><a href="">Mes réservations</a></li>
+            <li><a href="">Compte</a></li>
+        </ul>
+    </nav>
+</header>
+<!-- End header -->
+
+<!-- Hero Section -->
+<div class="section flex" id="hero-section">
+    <div class="text">
+        <h1 class="primary">
+            It's Not Just Food, <br />
+            It's an <span style="color: var(--clr-primary)">Experience...</span>
+        </h1>
+    </div>
+</div>
+<!-- End Hero Section -->
+
+<!-- How It Works -->
+<div class="section" id="how-it-works">
+    <h2 class="secondary">Nos Restaurants</h2>
+
+    <div class="container flex">
+        <div class="box">
+            <h3>La Tavola Felice</h3>
+            <img src="../image/italien.jpg" class="resto">
+            <p>
+                Vous êtes tenté par notre restaurant <strong>italien</strong>?
+                Cliquer ici pour commander !
+            </p>
+        </div>
+
+        <div class="box">
+            <h3>Mariachi Grill</h3>
+            <img src="../image/mexicain.jpg" class="resto">
+            <p>
+                Vous êtes tenté par notre restaurant <strong>mexicain</strong>?
+                Cliquer ici pour commander !
+            </p>
+        </div>
+
+        <div class="box">
+            <h3>La Folie des Papilles</h3>
+            <img src="../image/français.jpg" class="resto">
+            <p>
+                Vous êtes tenté par notre restaurant <strong>français</strong>?
+                Cliquer ici pour commander !
+            </p>
+        </div>
+
+        <div class="box">
+            <h3>Le Rêve du Panda</h3>
+            <img src="../image/chinois.jpg" class="resto">
+            <p>
+                Vous êtes tenté par notre restaurant <strong>chinois</strong>?
+                Cliquer ici pour commander !
+            </p>
+        </div>
+    </div>
+</div>
+<!-- End How It Works -->
+
+<!-- Footer -->
+<div class="footer">
+    <div class="container flex">
+        <div class="footer-about">
+            <h2>About</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+                aspernatur sit deleniti enim voluptas voluptatum incidunt rerum,
+                exercitationem voluptate nemo quo impedit ad perspiciatis tempore
+                nulla dolore fugit, fuga eos.
+            </p>
+        </div>
+
+        <div class="footer-category">
+            <h2>Our Menu</h2>
+
+            <ul>
+                <li>Biryani</li>
+                <li>Chicken</li>
+                <li>Pizza</li>
+                <li>Burger</li>
+                <li>Pasta</li>
+            </ul>
+        </div>
+
+        <div class="quick-links">
+            <h2>Quick Links</h2>
+
+            <ul>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Menu</li>
+                <li>Order</li>
+                <li>Services</li>
+            </ul>
+        </div>
+
+        <div class="get-in-touch">
+            <h2>Get in touch</h2>
+            <ul>
+                <li>Account</li>
+                <li>Support Center</li>
+                <li>Feedback</li>
+                <li>Suggession</li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="copyright">
+        <p>Copyright &copy; 2022. All Rights Reserved.</p>
+    </div>
+</div>
+<!-- End Footer -->
+
+</body>
+</html>
