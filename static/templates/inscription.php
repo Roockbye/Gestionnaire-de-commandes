19f4f4dbf8eeb8318147c4f60519b8cffe6e4f4c
Melanieynov <marmande.melanie@gmail.com>
2024-03-07 17:14:42 +0100

    page inscription/connexion

---
+++ static/templates/inscription.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Page d'inscription/connexion</title>
+  <link rel="stylesheet" href="../css/inscription.css">
+</head>
+<body>
+  <div class="wrapper">
+    <div class="form8">
+
+      <button class="form8__btncircle">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+
+      <div class="form8__log">
+        <span class="form8__text">
+          <span>Se connecter</span>
+        </span>
+        <form action="#">
+          <input type="text" name="mail" value="" autocomplete="off" class="form8__inpt" placeholder="Mail">
+          <input type="password" name="mdp" value="" autocomplete="off" class="form8__inpt" placeholder="Mot de passe">
+          <button class="form8__btn">Connexion</button>
+        </form>
+      </div>
+
+      <div class="form8__reg">
+        <span class="form8__text">
+          <span>S'inscrire</span>
+        </span>
+        <form action="#">
+          <input type="text" name="mail" value="" autocomplete="off" class="form8__inpt" placeholder="Mail">
+          <input type="password" name="mdp" value="" autocomplete="off" class="form8__inpt" placeholder="Mot de passe">
+          <input type="password" name="mdp-conf" value="" autocomplete="off" class="form8__inpt" placeholder="Confirmation mot de passe">
+          <button class="form8__btn">Confirmer</button>
+        </form>
+      </div>
+    </div>
+  </div>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script>
+    $('.form8__btncircle').click(function () {
+	$(this).toggleClass('active');
+	$('.form8').toggleClass('register__bg');
+	$('.form8__log, .form8__reg').toggleClass('slided');
+	if ($('.form8__reg').hasClass("slided")) {
+		$('.form8').animate({
+				'height': $('.form8__reg').height()
+			}, 10);
+	} else if (!$('.form8__reg').hasClass("slided")) {
+		$('.form8').animate({
+				'height': $('.form8__log').height()
+			}, 10);
+	}
+});
+  </script>
+</body>
+</html>
